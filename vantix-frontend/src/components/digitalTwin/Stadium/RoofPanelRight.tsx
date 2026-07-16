9c8641ec565a1c9c8ffbbd965cd9338b17301591
shan <shaandadhich1@gmail.com>
2026-07-16 11:50:06 +0530

    fix: adjust modular RoofPanelLeft and RoofPanelRight rotations to -Math.PI / 2 to position panels above the arches

--- vantix-frontend/src/components/digitalTwin/Stadium/RoofPanelRight.tsx
+++ vantix-frontend/src/components/digitalTwin/Stadium/RoofPanelRight.tsx
@@ -9,7 +9,7 @@ export const RoofPanelRight: React.FC<RoofPanelRightProps> = ({ color }) => {
   return (
     <group>
       {/* 1. Curved Roof Shell */}
-      <mesh rotation={[Math.PI / 2, 0, 0]}>
+      <mesh rotation={[-Math.PI / 2, 0, 0]}>
         <cylinderGeometry args={[23.62, 23.62, 8.0, 64, 1, true, Math.PI * 0.5, Math.PI * 0.06]} />
         <meshStandardMaterial
           color={color}
@@ -21,14 +21,14 @@ export const RoofPanelRight: React.FC<RoofPanelRightProps> = ({ color }) => {
 
       {/* 2. Structural Edge Beams (Details) */}
       {[-4.0, 0, 4.0].map((z, idx) => (
-        <mesh key={idx} position={[0, 0, z]} rotation={[Math.PI / 2, 0, 0]}>
+        <mesh key={idx} position={[0, 0, z]} rotation={[-Math.PI / 2, 0, 0]}>
           <cylinderGeometry args={[23.65, 23.65, 0.12, 64, 1, true, Math.PI * 0.5, Math.PI * 0.065]} />
           <meshStandardMaterial color="#475569" metalness={0.8} roughness={0.2} />
         </mesh>
       ))}
 
       {/* 3. Cross truss reinforcement lines (visual layout) */}
-      <mesh rotation={[Math.PI / 2, 0, 0]} position={[0, 0.01, 0]}>
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.01, 0]}>
         <cylinderGeometry args={[23.64, 23.64, 7.9, 8, 1, true, Math.PI * 0.5, Math.PI * 0.06]} />
         <meshStandardMaterial
           color="#334155"
